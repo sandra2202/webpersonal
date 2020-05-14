5a4b940e3c58dd90ef28c4517f4857cf279925e6
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-14 13:24:26 -0500

    Solucion crear los templates about portfolio y contact

--- core/views.py
+++ core/views.py
@@ -1,39 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base ="""
-<h1>Mi Web Personal Sandra</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="about-me">Acerca de </a></li>
-    <li><a href="contact">Contacto</a></li>
-    <li><a href="portfolio">Portafolio </a></li>
-   
-</ul>
-
-"""
-
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
-
+    
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Hola mi nombre es Sandra Gutierrez, me gusta programar y estoy apfrendiendo Django</p>
-    """)
-def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aquí dejo mi email y mis redes sociales</p>
-        <ul>
-            <li><a href="sandra2202gutierrez@gmail.com">Email</a></li>
-            <li><a href="https://youtube.com"></a>Facebook</li>
-        </ul>
+    return render(request, "core/about.html")
 
-    """)
+def contact(request):
+    return render(request, "core/contact.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-        <h2>Portafolio</h2>
-        <p>Algunos de mis trabajos </p>
-    """)
+    return render(request, "core/portfolio.html")
