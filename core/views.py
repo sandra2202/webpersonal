3b516472eb6216eaca99b6d080aabe8f83912d15
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-14 12:52:00 -0500

    Solucion pagina portafolio y contacto

--- core/views.py
+++ core/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render, HttpResponse
 
+html_base ="""
+<h1>Mi Web Personal Sandra</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="about-me">Acerca de </a></li>
+    <li><a href="contact">Contacto</a></li>
+    <li><a href="portfolio">Portafolio </a></li>
+   
+</ul>
+
+"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Mi Web Personal Sandra</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """
+        <h2>Bienvenidos a la portada</h2>
+        <p>Esto es la portada.</p>
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Hola mi nombre es Sandra Gutierrez, me gusta programar y estoy apfrendiendo Django</p>
+    """)
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí dejo mi email y mis redes sociales</p>
+        <ul>
+            <li><a href="sandra2202gutierrez@gmail.com">Email</a></li>
+            <li><a href="https://youtube.com"></a>Facebook</li>
+        </ul>
+
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Portafolio</h2>
+        <p>Algunos de mis trabajos </p>
+    """)
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from core import views
 urlpatterns = [
     path('',views.home, name="home"),
+    path('about-me',views.about, name="about"),
+    path('contact',views.contact, name="contact"),
+    path('portfolio',views.portfolio, name="portfolio"),
     path('admin/', admin.site.urls),
 
 ]
