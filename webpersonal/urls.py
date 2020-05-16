471d4f9a607c1e375a31f3fcecc028dafd8194ef
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-15 19:40:47 -0500

    Personalizando el administrador

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+
+from django.conf import settings
+
 urlpatterns = [
     path('',views.home, name="home"),
     path('about-me',views.about, name="about"),
     path('contact',views.contact, name="contact"),
     path('portfolio',views.portfolio, name="portfolio"),
     path('admin/', admin.site.urls),
-
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
